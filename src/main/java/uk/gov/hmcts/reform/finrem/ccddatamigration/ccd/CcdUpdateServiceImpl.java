c942fc3bf79dc4b6aa63797466a2f8123aec5925
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2024-03-08 10:01:05 +0000

    Remove update case data code (#24)

--- src/main/java/uk/gov/hmcts/reform/finrem/ccddatamigration/ccd/CcdUpdateServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/ccddatamigration/ccd/CcdUpdateServiceImpl.java
@@ -1,11 +1,7 @@
 package uk.gov.hmcts.reform.finrem.ccddatamigration.ccd;
 
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.ccd.client.model.Event;
-import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
-import uk.gov.hmcts.reform.finrem.ccddatamigration.domain.UserDetails;
 
 @Service
 public class CcdUpdateServiceImpl extends BaseCcdCaseService implements CcdUpdateService {
@@ -13,36 +9,6 @@ public class CcdUpdateServiceImpl extends BaseCcdCaseService implements CcdUpdat
     @Override
     public CaseDetails update(String caseId, Object data, String eventId, String authorisation,
                               String eventSummary, String eventDescription, String caseType) {
-        UserDetails userDetails = getUserDetails(authorisation);
-
-        StartEventResponse startEventResponse = coreCaseDataApi.startEventForCaseWorker(
-                getBearerUserToken(authorisation),
-                getServiceAuthToken(),
-                userDetails.getId(),
-                jurisdictionId,
-                caseType,
-                caseId,
-                eventId);
-
-        CaseDataContent caseDataContent = CaseDataContent.builder()
-                .eventToken(startEventResponse.getToken())
-                .event(
-                        Event.builder()
-                                .id(startEventResponse.getEventId())
-                                .summary(eventSummary)
-                                .description(eventDescription)
-                                .build()
-                ).data(data)
-                .build();
-
-        return coreCaseDataApi.submitEventForCaseWorker(
-                getBearerUserToken(authorisation),
-                getServiceAuthToken(),
-                userDetails.getId(),
-                jurisdictionId,
-                caseType,
-                caseId,
-                true,
-                caseDataContent);
+        throw new UnsupportedOperationException("See DFR-2863");
     }
 }
